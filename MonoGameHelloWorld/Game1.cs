e34cbc6d995022cbe096e8548fc450dce82907a7
ETdoFresh <ETdoFresh@gmail.com>
2024-12-31 19:13:52 -0600

    Remove freetype files from project root

--- MonoGameHelloWorld/Game1.cs
+++ MonoGameHelloWorld/Game1.cs
@@ -1,4 +1,4 @@
-﻿﻿using Microsoft.Xna.Framework;
+﻿﻿﻿﻿﻿﻿﻿﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,6 +9,7 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private Texture2D _whitePixel;
+    private SpriteFont _font;
 
     public Game1()
     {
@@ -29,6 +30,9 @@ public class Game1 : Game
         // Create a 1x1 white pixel texture
         _whitePixel = new Texture2D(GraphicsDevice, 1, 1);
         _whitePixel.SetData(new[] { Color.White });
+        
+        // Load the default font
+        _font = Content.Load<SpriteFont>("DefaultFont");
     }
 
     protected override void Update(GameTime gameTime)
@@ -48,6 +52,14 @@ public class Game1 : Game
         // Draw a simple rectangle
         var rect = new Rectangle(100, 100, 200, 100);
         _spriteBatch.Draw(_whitePixel, rect, Color.Green);
+        
+        // Draw Hello World centered on screen
+        string text = "Hello World";
+        Vector2 textSize = _font.MeasureString(text);
+        Vector2 textPosition = new Vector2(
+            (GraphicsDevice.Viewport.Width - textSize.X) / 2,
+            (GraphicsDevice.Viewport.Height - textSize.Y) / 2);
+        _spriteBatch.DrawString(_font, text, textPosition, Color.White);
         
         _spriteBatch.End();
 
