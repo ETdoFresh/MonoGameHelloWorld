ca2b340dea968b1c40345c34a78291a932a8bc52
ETdoFresh <ETdoFresh@gmail.com>
2024-12-31 19:09:44 -0600

    Set up basic MonoGame project with rectangle drawing

---
+++ MonoGameHelloWorld/Game1.cs
@@ -0,0 +1,56 @@
+﻿﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameHelloWorld;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D _whitePixel;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
+        // Create a 1x1 white pixel texture
+        _whitePixel = new Texture2D(GraphicsDevice, 1, 1);
+        _whitePixel.SetData(new[] { Color.White });
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+        
+        // Draw a simple rectangle
+        var rect = new Rectangle(100, 100, 200, 100);
+        _spriteBatch.Draw(_whitePixel, rect, Color.Green);
+        
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
